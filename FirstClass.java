40e82f0ae3f1c4535ac3f5400faef31df0601103
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-06 23:49:57 +0530

    variables in java

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,32 @@
 class FirstClass {
     public static void main(String args[]) {
-        System.out.println("Hello, World!");
+        // String name = "John Doe";
+        int age = 25;
+        // Float height = 5.9f;
+        // Character initial = 'J';
+        // System.out.println("Hello, " + name + "! Welcome to Java programming.");
+        System.out.println("You are " + age + " years old.");
+        // System.out.println("Your height is " + height + " feet.");
+        // System.out.println("Your initial is " + initial + ".");
+        
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+        // System.out.println("*****");
+        
+        //* Print a star * and two star on next line
+
+        // System.out.print("*\n");
+        // System.out.print("**\n");
+
+        int a = 2;
+        int b = 5;
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+        
+        
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Java-Repo
 A code repo for Java - for beginner
+
+1. Variables
+* A variable is a container (storage area) used to hold data.  
+* Each variable should be given a unique name (identifier). 
+
+```
+class Main {
+
+   public static void main(String[] args) {
+      // Variables
+       String name = "Aman";
+       int age = 30;
+
+       String neighbour = "Akku";
+       String friend = neighbour;
+   }
+}
+
+```
