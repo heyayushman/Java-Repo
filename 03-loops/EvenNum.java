b492b4e948df8f112c74c88d7143298d7901cbb4
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-10 10:26:24 +0530

    mark calculation using for loop

---
+++ 03-loops/EvenNum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class EvenNum {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i = 1; i <= num; i++){
+            if(i % 2 == 0){
+                System.out.print(i +" ");
+            }
+        }   
+    }
+}
--- 03-loops/Loop.java
+++ 03-loops/Loop.java
@@ -1,24 +1,41 @@
+
 import java.util.*;
+
 class Loop {
+
     public static void main(String[] args) {
 
         //? for loop
-
-        System.out.print("Enter a number: ");
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for(int i = 1; i <= 10; i++){
-           System.out.println(num + " * " + i + " = " + (num * i));
-        }
-
+        // System.out.print("Enter a number: ");
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        // for(int i = 1; i <= 10; i++){
+        //    System.out.println(num + " * " + i + " = " + (num * i));
+        // }
         //? while loop
-
         // int i = 1;
         // while(i <= 100){
         //     System.out.println("Pj is PANDU");
         //     i++;
         // }
-
-
+//? taking marks
+        System.out.print("Enter 1 or 0 : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num == 1) {
+            System.out.print("Enter marks : ");
+            int marks = sc.nextInt();
+            if (marks >= 90) {
+                System.out.println("This is Good");
+            } else if (marks >= 89 && marks >= 60) {
+                System.out.println("This is also Good");
+            } else if (marks >= 59 && marks >= 0) {
+                System.out.println("This is Good as well");
+            } else {
+                System.out.println("This is Good too");
+            }
+        } else {
+            System.out.println("Stopped");
+        }
     }
 }
