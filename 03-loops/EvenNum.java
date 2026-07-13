5c9c06743cf45ea53870125713c60130cc50fafa
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-13 09:13:50 +0530

    loop in java

--- 03-loops/EvenNum.java
+++ 03-loops/EvenNum.java
@@ -7,7 +7,7 @@ class EvenNum {
         int num = sc.nextInt();
         for(int i = 1; i <= num; i++){
             if(i % 2 == 0){
-                System.out.print(i +" ");
+                System.out.print(i + " ");
             }
         }   
     }
