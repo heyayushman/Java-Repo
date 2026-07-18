c537805ea4321e88b90aebd4e4cff3cb85d76c73
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-18 23:18:34 +0530

    function in Java

--- 04-functions/Function.java
+++ 04-functions/Function.java
@@ -1,18 +1,14 @@
 
-import java.util.Scanner;
-
-
-public class Function {
-    public static void printName(String name){
-        System.err.println("Hello " + name );
-    }
-    public static void main(String[] args){
-        System.err.print("Enter your name : ");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printName(name);
-    }
-}
+//     public static void printName(String name){
+//         System.err.println("Hello " + name );
+//     }
+//     public static void main(String[] args){
+//         System.err.print("Enter your name : ");
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+//         printName(name);
+//     }
+// }
 
 /**
 //*Method	    Description
@@ -25,3 +21,17 @@ nextLine()	    Reads a String value from the user
 nextLong()	    Reads a long value from the user
 nextShort()	    Reads a short value from the user
  */
+
+public class Function{
+    public static void Arithmetic(int a, int b){
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Sub = " + (a - b));
+        System.out.println("Multiply = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Mod = " + (a % b));
+    }
+    public static void main(String[] args){
+        int a = 20, b = 5;
+        Arithmetic(a, b);
+    }
+}
