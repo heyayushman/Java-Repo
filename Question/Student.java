dcde3fd48e0b99fb57da91c849c622a3311d6f4c
Ayushman Baitha <ayushmanbaitha4@gmail.com>
2026-07-21 20:44:33 +0530

    Student information details in Java

---
+++ Question/Student.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Student{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();
+        
+        System.out.print("Enter student USN: ");
+        int usn = sc.nextInt();
+        
+        System.out.print("Enter student age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter student course: ");
+        String course = sc.next();
+
+        System.out.println("Student Details");
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Student USN: " + usn);
+        System.out.println("Student Age: " + age);
+        System.out.println("Student Course: " + course);
+
+
+    }
+}
